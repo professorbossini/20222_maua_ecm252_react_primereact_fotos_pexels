362e2a504e293fb1577bb89ab1abae531a39b035
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-01 10:32:03 -0300

    feat(apostila07): componente busca se torna controlado, pagina 12

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,14 @@ import Busca from './Busca'
 
 const App = () => {
   return (
-    <div>
-      <Busca />
+    <div className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
+      {/* .col-12 */}
+      <div className="col-12">
+        <h1>Exibir uma lista de...</h1>
+      </div>
+      <div className="col-8">
+        <Busca />
+      </div>
     </div>
   )
 }
